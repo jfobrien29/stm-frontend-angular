f8ea5601150a202f6ac9d8ac6dbcdf07c9bd9191
jfobrien29 <thejackobrien29@gmail.com>
2019-09-05 20:01:42 -0400

    Add firebase config

--- src/app/models/link.ts
+++ src/app/models/link.ts
@@ -6,18 +6,18 @@ export interface SimpleLink {
 
 export const SAMPLE_LINKS: SimpleLink[]  = [
     {
-        url: 'http://www.google.com',
-        title: 'Google',
+        url: 'https://www.wired.com/story/trump-tweeted-a-sensitive-photo-internet-sleuths-decoded-it/',
+        title: 'Trump drone story',
         avgTimeInSeconds: 500,
     },
     {
-        url: 'http://www.nyt.com',
-        title: 'New York Times',
+        url: 'https://www.nytimes.com/2019/09/04/world/americas/bahamas-stunned-as-water-recedes-its-like-a-bomb-went-off.html?action=click&module=Top%20Stories&pgtype=Homepage',
+        title: 'Dorian, Bahamas',
         avgTimeInSeconds: 400,
     },
     {
-        url: 'http://www.medium.com',
-        title: 'Medium',
+        url: 'https://onezero.medium.com/how-to-ensure-our-digital-legacy-isnt-lost-to-the-future-f6a226bc6792',
+        title: 'Bit rot',
         avgTimeInSeconds: 180,
     }
 ]
