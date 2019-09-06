3bc4c26591b2d3c3e4bee9613864d52a41554cf3
jfobrien29 <thejackobrien29@gmail.com>
2019-09-06 00:04:56 -0400

    Connected to database directly

--- src/app/services/link-service.service.ts
+++ src/app/services/link-service.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import {SimpleLink, SAMPLE_LINKS} from '../models/link';
+import { SimpleLink, SAMPLE_LINKS } from '../models/link';
+import { FirebaseService } from './firebase.service';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { DocumentChangeAction } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinkService {
-  getTargetLink(): Observable<SimpleLink> {
-    const randomLink = this.getRandomLink();
-    return of(randomLink);
+
+  constructor(private readonly fbService: FirebaseService) { }
+
+  getTargetLink(): Observable<string> {
+    return this.fbService.getLinks().pipe(map(links => this.getRandomFbLink(links)));
   }
 
-  private getRandomLink(): SimpleLink {
-    const randomIndex = Math.floor(Math.random() * SAMPLE_LINKS.length);
-    return SAMPLE_LINKS[randomIndex];
+  private getRandomFbLink(links: DocumentChangeAction<unknown>[]): string {
+    const randomIndex = Math.floor(Math.random() * links.length);
+    const linkDoc = links[randomIndex].payload.doc.data() as {link: string};
+    return linkDoc.link;
   }
 }
